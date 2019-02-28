c20e77e8dc635a268bb443bfbe8389ab952226a9
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-02-28 11:06:28 +0200

    Simplify

--- http.c
+++ http.c
@@ -4,14 +4,13 @@
 #include "http.h"
 
 void respond_http(const int sock) {
-	const char* data =
-	"HTTP/1.1 301 aem\r\n"
-	"TSV: N\r\n"
-	"Location: https://"AEM_DOMAIN"\r\n"
-	"Content-Length: 0\r\n"
-	"Connection: close\r\n"
-	"Strict-Transport-Security: max-age=99999999\r\n" // 3+ years
-	"\r\n";
-
-	send(sock, data, 131 + AEM_LEN_DOMAIN, 0);
+	send(sock, 
+		"HTTP/1.1 301 aem\r\n"
+		"TSV: N\r\n"
+		"Location: https://"AEM_DOMAIN"\r\n"
+		"Content-Length: 0\r\n"
+		"Connection: close\r\n"
+		"Strict-Transport-Security: max-age=99999999\r\n" // 3+ years
+		"\r\n";
+	, 131 + AEM_LEN_DOMAIN, 0);
 }
