3dd526567fe58c6c73a766b6dcb37b55bb167b72
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-06-04 21:18:06 +0300

    Add smtp.c and smtp.h

---
+++ smtp.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include "smtp.h"
+
+int respond_smtp(const int sock) {
+	return 0;
+}
---
+++ smtp.h
@@ -0,0 +1,6 @@
+#ifndef AEM_SMTP_H
+#define AEM_SMTP_H
+
+int respond_smtp(const int sock);
+
+#endif
