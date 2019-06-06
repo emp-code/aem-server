d830d9414eae514e78096826cd7cce74ee3308fd
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-06-07 01:32:46 +0300

    Minor cleanup

--- js/all-ears.js
+++ js/all-ears.js
@@ -6,7 +6,6 @@ function AllEars() {
 // Private
 	// Base64 encoded server public key for NaCl Box
 	const _serverPublicKey = b64ToBin("zUv7tx3dQU8vSq93dGOl6RSDv0N+6PDZbhOesYkx2zo=");
-	const _test = "heko"
 
 	var _userKeys;
 
@@ -45,6 +44,7 @@ function AllEars() {
 			var login_nonce = b64ToBin(b64_login_nonce);
 			const plaintext = new TextEncoder().encode("AllEars:Web.Login");
 			var box_login = nacl.box(plaintext, login_nonce, _serverPublicKey, _userKeys.secretKey);
+
 			var b64_box_login = btoa(String.fromCharCode.apply(null, box_login));
 
 			_Fetch("/web/login/" + b64_key_public + "." + b64_box_login, function(httpStatus, response) {
