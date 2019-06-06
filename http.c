23c3ef47c2d08d13fe64e3d9f25cb9157f142a30
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-06-06 21:34:07 +0300

    Correct Tk header name, STS length to 3 years

--- http.c
+++ http.c
@@ -6,11 +6,11 @@
 void respond_http(const int sock) {
 	send(sock, 
 		"HTTP/1.1 301 aem\r\n"
-		"TSV: N\r\n"
+		"Tk: N\r\n"
+		"Strict-Transport-Security: max-age=94672800\r\n"
 		"Location: https://"AEM_DOMAIN"\r\n"
 		"Content-Length: 0\r\n"
 		"Connection: close\r\n"
-		"Strict-Transport-Security: max-age=99999999\r\n" // 3+ years
 		"\r\n"
-	, 131 + AEM_LEN_DOMAIN, 0);
+	, 130 + AEM_LEN_DOMAIN, 0);
 }
