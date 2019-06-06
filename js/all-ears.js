f59965f3ba73508ef95de1a88ce3b0a6f3180e31
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-06-06 14:01:16 +0300

    Add and use AEM_fetch function

--- js/all-ears.js
+++ js/all-ears.js
@@ -7,6 +7,19 @@ const AllEars_serverPublicKey = b64ToBin("zUv7tx3dQU8vSq93dGOl6RSDv0N+6PDZbhOesY
 
 var AllEars_userKeys;
 
+function AEM_fetch(url, cb) {
+	var r=new XMLHttpRequest();
+
+	r.onreadystatechange=function(){
+		if (r.readyState == 4 && typeof(cb) === "function") {
+			cb(r.status, r.responseText);
+		}
+	}
+
+	r.open("GET", url);
+	r.send();
+}
+
 // Set user's keys from a Base64-encoded secret key
 function AllEars_SetKeys(skey_b64) {
 	AllEars_userKeys=nacl.box.keyPair.fromSecretKey(b64ToBin(skey_b64));
@@ -19,26 +32,25 @@ function AllEars_NewKeys() {
 }
 
 function AllEars_Login() {
-	//TODO: Get login nonce from server (24 bytes binary)
-	var login_nonce_b64 = "1rE+qiwudzOo5HWHyff+0S6QgbjqekCP";
-	var login_nonce = b64ToBin(login_nonce_b64);
-	var plaintext = new TextEncoder().encode("AllEars:Web.Login");
-
-	var box_login = nacl.box(plaintext, login_nonce, AllEars_serverPublicKey, AllEars_userKeys.secretKey);
-
+	const plaintext = new TextEncoder().encode("AllEars:Web.Login");
 	var b64_key_public = btoa(String.fromCharCode.apply(null, AllEars_userKeys.publicKey));
-	var b64_box_login = btoa(String.fromCharCode.apply(null, box_login));
 
-	r=new XMLHttpRequest();
-	r.open("GET", "https://allears.test:60443/web/login/" + b64_key_public + "." + b64_box_login);
-	r.send();
-	r.onreadystatechange = function() {
-		if (r.readyState == 4) {
-			if (r.status == 200) {
+	AEM_fetch("https://allears.test:60443/web/nonce/" + b64_key_public, function(httpStatus, b64_login_nonce) {
+		if (httpStatus != 200) {
+			console.log("Failed to get nonce from server");
+			return;
+		}
+
+		var login_nonce = b64ToBin(b64_login_nonce);
+		var box_login = nacl.box(plaintext, login_nonce, AllEars_serverPublicKey, AllEars_userKeys.secretKey);
+		var b64_box_login = btoa(String.fromCharCode.apply(null, box_login));
+
+		AEM_fetch("https://allears.test:60443/web/login/" + b64_key_public + "." + b64_box_login, function(httpStatus, response) {
+			if (httpStatus == 200) {
 				console.log("Login: Success");
 			} else {
 				console.log("Login: Failure");
 			}
-		}
-	};
+		});
+	});
 }
