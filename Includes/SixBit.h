edd2f1b0cb510e417456e434e1ae8613ee5c0aca
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-06-10 20:17:36 +0300

    Minor cleanup

--- Includes/SixBit.h
+++ Includes/SixBit.h
@@ -1,7 +1,7 @@
 #ifndef AEM_INCLUDES_SIXBIT_H
 #define AEM_INCLUDES_SIXBIT_H
 
-char *sixBitToText(const char* source, const size_t lenSource);
+char *sixBitToText(const char *source, const size_t lenSource);
 char *textToSixBit(const char *source, const size_t lenSource);
 
 #endif
