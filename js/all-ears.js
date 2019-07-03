cfaf60ed3b7c08e31c8576518fc6b165ecd101e9
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-07-03 22:41:52 +0300

    Refuse to run script if document is in quirks mode

--- js/all-ears.js
+++ js/all-ears.js
@@ -2,6 +2,7 @@
 
 function AllEars() {
 	if (!document.documentURI.startsWith("https://")) return;
+	if (document.compatMode == "BackCompat") return;
 
 // Private
 	const _serverPkHex = "0f4d188b9cd0b9a675d947d34eee8dd119522736f498fdc137dd70cec9494d5a"; // Server public key in hex
