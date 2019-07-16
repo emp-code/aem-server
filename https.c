f8255883300a629238d427001213d509c53daa3b
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-07-16 23:27:31 +0300

    Only support strong ciphersuites

--- https.c
+++ https.c
@@ -33,6 +33,12 @@
 
 #define AEM_NONCE_TIMEDIFF_MAX 3
 
+#define AEM_CIPHERSUITES {\
+MBEDTLS_TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,\
+MBEDTLS_TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,\
+MBEDTLS_TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,\
+MBEDTLS_TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256}
+
 // Server keypair for testing (Base64)
 // Public: D00Yi5zQuaZ12UfTTu6N0RlSJzb0mP3BN91wzslJTVo=
 // Secret: tCpcTrVsxFRiL8z8+g1SclyHsfX1KSmYZLIA21cHROg=
@@ -758,6 +764,8 @@ int respond_https(int sock, mbedtls_x509_crt *srvcert, mbedtls_pk_context *pkey,
 
 	mbedtls_ssl_conf_min_version(&conf, MBEDTLS_SSL_MAJOR_VERSION_3, MBEDTLS_SSL_MINOR_VERSION_3); // Require TLS v1.2+
 	mbedtls_ssl_conf_dhm_min_bitlen(&conf, 4096); // Minimum length for DH parameters
+	const int cs[] = AEM_CIPHERSUITES;
+	mbedtls_ssl_conf_ciphersuites(&conf, cs);
 
 	// Seed the RNG
 	mbedtls_ctr_drbg_context ctr_drbg;
