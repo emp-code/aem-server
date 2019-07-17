66ed2f467cf53332d9a1a44f6cd329687a962c08
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-07-17 16:39:42 +0300

    Remove HSTS from HTTP response

--- http.c
+++ http.c
@@ -5,20 +5,19 @@
 
 void respond_http(const int sock, const char * const domain) {
 	const size_t len = strlen(domain);
-	char r[130 + len];
+	char r[85 + len];
 
 	memcpy(r,
 		"HTTP/1.1 301 aem\r\n"
 		"Tk: N\r\n"
-		"Strict-Transport-Security: max-age=94672800\r\n"
 		"Content-Length: 0\r\n"
 		"Connection: close\r\n"
 		"Location: https://"
-	, 126);
+	, 81);
 
-	memcpy(r + 126, domain, len);
-	memcpy(r + 126 + len, "\r\n\r\n", 4);
+	memcpy(r + 81, domain, len);
+	memcpy(r + 81 + len, "\r\n\r\n", 4);
 
-	write(sock, r, 130 + len);
+	write(sock, r, 85 + len);
 	close(sock);
 }
