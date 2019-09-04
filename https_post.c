218a35b924c0d6e1614148b796dbbdcb88d74d37
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-09-04 08:58:26 +0300

    https_post: Require URL length to be 14

--- https_post.c
+++ https_post.c
@@ -378,7 +378,7 @@ static char *openWebBox(const unsigned char * const post, const size_t lenPost, 
 
 void https_post(mbedtls_ssl_context * const ssl, const unsigned char * const ssk, const unsigned char * const addrKey,
 const char * const domain, const size_t lenDomain, const char * const url, const size_t lenUrl, const unsigned char * const post, const size_t lenPost) {
-	if (lenUrl < 4) return;
+	if (lenUrl != 14) return;
 
 	unsigned char upk[crypto_box_PUBLICKEYBYTES];
 	size_t lenDecrypted;
