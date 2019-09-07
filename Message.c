402beac2e03ef1566d94a2159b901a582b0543db
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-09-07 09:16:52 +0300

    msg_makeBodyBox: Better way of calculating padded len without libmath

--- Message.c
+++ Message.c
@@ -2,7 +2,6 @@
 #include <stdbool.h>
 #include <string.h>
 #include <time.h>
-#include <math.h> // for ceil
 
 #include <sodium.h>
 
@@ -79,7 +78,7 @@ TextNote/FileNote
 */
 
 static unsigned char *msg_makeBodyBox(const unsigned char * const pk, const char * const bodyText, size_t * const bodyLen) {
-	const size_t bodyLenPadded = ceil(*bodyLen / (double)1024) * 1024;
+	const size_t bodyLenPadded = ((*bodyLen - (*bodyLen % 1024)) / 1024) + 1;
 	const size_t padLen = bodyLenPadded - *bodyLen;
 
 	const uint16_t padLen16 = padLen;
