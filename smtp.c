0cb83546c0a472888863a1be4dc50698be6a58da
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-09-23 00:42:18 +0300

    unfoldHeaders: Don't search past headers

--- smtp.c
+++ smtp.c
@@ -591,14 +591,19 @@ static uint8_t getTlsVersion(const mbedtls_ssl_context * const tls) {
 }
 
 void unfoldHeaders(char * const * const data, size_t * const lenData) {
+	const char * const headersEnd = memmem(*data, *lenData, "\r\n\r\n", 4);
+	if (headersEnd == NULL) return;
+	size_t lenSearch = headersEnd - *data;
+
 	while(1) {
-		char *crlfWsp = memmem(*data, *lenData, "\r\n ", 3);
-		if (crlfWsp == NULL) crlfWsp = memmem(*data, *lenData, "\r\n\t", 3);
+		char *crlfWsp = memmem(*data, lenSearch, "\r\n ", 3);
+		if (crlfWsp == NULL) crlfWsp = memmem(*data, lenSearch, "\r\n\t", 3);
 		if (crlfWsp == NULL) break;
 
 		memmove(crlfWsp, crlfWsp + 2, (*data + *lenData) - (crlfWsp + 2));
 
 		*lenData -= 2;
+		lenSearch -= 2;
 		(*data)[*lenData] = '\0';
 	}
 }
