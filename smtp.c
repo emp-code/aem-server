1d16613aa92101f2c98aa550819dcefc4a569f73
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-09-25 18:26:57 +0300

    respond_smtp: Cleanup to TLS section

--- smtp.c
+++ smtp.c
@@ -502,14 +502,17 @@ void respond_smtp(int sock, mbedtls_x509_crt * const tlsCert, mbedtls_pk_context
 	mbedtls_ssl_context *tls = NULL;
 	mbedtls_ssl_context ssl;
 	mbedtls_ssl_config conf;
-	mbedtls_ctr_drbg_context ctr_drbg;
 	mbedtls_entropy_context entropy;
+	mbedtls_ctr_drbg_context ctr_drbg;
 
 	if (bytes >= 8 && strncasecmp(buf, "STARTTLS", 8) == 0) {
 		send(sock, "220 aem\r\n", 9, 0);
 		tls = &ssl;
 
+		mbedtls_ssl_init(tls);
 		mbedtls_ssl_config_init(&conf);
+		mbedtls_entropy_init(&entropy);
+		mbedtls_ctr_drbg_init(&ctr_drbg);
 
 		int ret;
 		if ((ret = mbedtls_ssl_config_defaults(&conf, MBEDTLS_SSL_IS_SERVER, MBEDTLS_SSL_TRANSPORT_STREAM, MBEDTLS_SSL_PRESET_DEFAULT)) != 0) {
@@ -519,20 +522,17 @@ void respond_smtp(int sock, mbedtls_x509_crt * const tlsCert, mbedtls_pk_context
 			return;
 		}
 
+		mbedtls_ssl_conf_arc4_support(&conf, MBEDTLS_SSL_ARC4_ENABLED);
+		mbedtls_ssl_conf_ciphersuites(&conf, smtp_ciphersuites);
 		mbedtls_ssl_conf_min_version(&conf, MBEDTLS_SSL_MAJOR_VERSION_3, MBEDTLS_SSL_MINOR_VERSION_1); // Require TLS v1.0+
 		mbedtls_ssl_conf_read_timeout(&conf, AEM_SMTP_TIMEOUT);
-		mbedtls_ssl_conf_ciphersuites(&conf, smtp_ciphersuites);
-		mbedtls_ssl_conf_arc4_support(&conf, MBEDTLS_SSL_ARC4_ENABLED);
+		mbedtls_ssl_conf_rng(&conf, mbedtls_ctr_drbg_random, &ctr_drbg);
 
-		mbedtls_ctr_drbg_init(&ctr_drbg);
-		mbedtls_entropy_init(&entropy);
 		if ((ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, (unsigned char*)"All-Ears Mail SMTP", 18)) != 0) {
 			printf("[SMTP] Terminating: mbedtls_ctr_drbg_seed returned %d\n", ret);
 			tlsFree(tls, &conf, &ctr_drbg, &entropy);
 			return;
 		}
-
-		mbedtls_ssl_conf_rng(&conf, mbedtls_ctr_drbg_random, &ctr_drbg);
 
 		mbedtls_ssl_conf_ca_chain(&conf, tlsCert->next, NULL);
 		if ((ret = mbedtls_ssl_conf_own_cert(&conf, tlsCert, tlsKey)) != 0) {
@@ -540,8 +540,6 @@ void respond_smtp(int sock, mbedtls_x509_crt * const tlsCert, mbedtls_pk_context
 			tlsFree(tls, &conf, &ctr_drbg, &entropy);
 			return;
 		}
-
-		mbedtls_ssl_init(tls);
 
 		if ((ret = mbedtls_ssl_setup(tls, &conf)) != 0) {
 			printf("[SMTP] Terminating: mbedtls_ssl_setup returned %d\n", ret);
