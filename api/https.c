501f6f4968549ead46bf33e14cb5a0e4e1efb64e
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-10-24 08:11:54 +0300

    getRequestType: Disallow same-origin instead of same-site

--- api/https.c
+++ api/https.c
@@ -91,7 +91,7 @@ static int getRequestType(char * const req, size_t lenReq, const char * const do
 		|| (strcasestr(req, "\r\nCookie:") != NULL)
 		|| (strcasestr(req, "\r\nExpect:") != NULL)
 		|| (strcasestr(req, "\r\nRange:")  != NULL)
-		|| (strcasestr(req, "\r\nSec-Fetch-Site: same-site") != NULL)
+		|| (strcasestr(req, "\r\nSec-Fetch-Site: same-origin") != NULL)
 		// These are only for preflighted requests, which All-Ears doesn't use
 		|| (strcasestr(req, "\r\nAccess-Control-Request-Method:")  != NULL)
 		|| (strcasestr(req, "\r\nAccess-Control-Request-Headers:") != NULL)
