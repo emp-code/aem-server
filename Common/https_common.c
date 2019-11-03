cb58c15716ac63a2757a4244d07eb81c5adfc9d6
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-11-03 14:39:25 +0200

    Refuse to send null or zero-size data

--- Common/https_common.c
+++ Common/https_common.c
@@ -1,6 +1,8 @@
 #include <mbedtls/ssl.h>
 
 void sendData(mbedtls_ssl_context * const ssl, const void * const data, const size_t lenData) {
+	if (data == NULL || lenData < 1) return;
+
 	size_t sent = 0;
 
 	while (sent < lenData) {
