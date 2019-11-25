9fdf7be5d2f4b471b8625b2ae526fe308f890241
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-11-25 11:44:34 +0200

    Process linebreaks as well

--- Common/QuotedPrintable.c
+++ Common/QuotedPrintable.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <stdlib.h>
 #include <string.h>
 
@@ -10,6 +12,24 @@ static unsigned char hexToChar(const char * const src) {
 }
 
 void decodeQuotedPrintable(char * const data, size_t * const lenData) {
+	while(1) {
+		char * c = memmem(data, *lenData, "=\r\n", 3);
+		if (c == NULL) break;
+
+		const char * const copyFrom = c + 3;
+		memmove(c, copyFrom, *lenData - (copyFrom - data));
+		*lenData -= 3;
+	}
+
+	while(1) {
+		char * c = memmem(data, *lenData, "=\n", 2);
+		if (c == NULL) break;
+
+		const char * const copyFrom = c + 2;
+		memmove(c, copyFrom, *lenData - (copyFrom - data));
+		*lenData -= 2;
+	}
+
 	while(1) {
 		char * const enc = memchr(data, '=', *lenData - 1);
 		if (enc == NULL) break;
