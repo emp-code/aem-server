f5069717639ce5fdedc5052ec14689a4b11064d4
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-12-10 17:50:47 +0200

    Simplify functions

--- Common/Trim.c
+++ Common/Trim.c
@@ -3,59 +3,38 @@
 #include <stddef.h>
 #include <string.h>
 
-// Convert non-breaking space to normal space (UTF-8)
-void convertNbsp(char * const text, size_t * const len) {
+static void convertText(char * const text, size_t * const len, const char * const bad, const size_t lenBad, const char good) {
 	while(1) {
-		char * const c = memmem(text, *len, "\xc2\xa0", 2);
+		char * const c = memmem(text, *len, bad, lenBad);
 		if (c == NULL) break;
 
 		memmove(c, c + 1, (text + *len) - (c + 1));
-		*len -= 1;
-		*c = ' ';
+		(*len)--;
+		*c = good;
 	}
+}
+
+// Convert non-breaking space to normal space (UTF-8)
+void convertNbsp(char * const text, size_t * const len) {
+	convertText(text, len, "\xc2\xa0", 2, ' ');
 }
 
 // Compress multiple spaces to one
 void trimSpace(char * const text, size_t * const len) {
-	while(1) {
-		char * const c = memmem(text, *len, "  ", 2);
-		if (c == NULL) break;
-
-		memmove(c, c + 1, (text + *len) - (c + 1));
-		*len -= 1;
-	}
+	convertText(text, len, "  ", 2, ' ');
 }
 
 // Remove space before linebreak
 void removeSpaceEnd(char * const text, size_t * const len) {
-	while(1) {
-		char * const c = memmem(text, *len, " \n", 2);
-		if (c == NULL) break;
-
-		memmove(c, c + 1, (text + *len) - (c + 1));
-		*len -= 1;
-	}
+	convertText(text, len, " \n", 2, '\n');
 }
 
 // Remove space after linebreak
 void removeSpaceBegin(char * const text, size_t * const len) {
-	while(1) {
-		char * const c = memmem(text, *len, "\n ", 2);
-		if (c == NULL) break;
-
-		memmove(c, c + 1, (text + *len) - (c + 1));
-		*len -= 1;
-		*c = '\n';
-	}
+	convertText(text, len, "\n ", 2, '\n');
 }
 
 // Compress over two linebreaks spaces to two
 void trimLinebreaks(char * const text, size_t * const len) {
-	while (1) {
-		char * const c = memmem(text, *len, "\n\n\n", 3);
-		if (c == NULL) break;
-
-		memmove(c, c + 1, (text + *len) - (c + 1));
-		*len -= 1;
-	}
+	convertText(text, len, "\n\n\n", 3, '\n');
 }
