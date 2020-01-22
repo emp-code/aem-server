383df8f71a2649e53ea024dc7aca557b14a1a208
EMPcode <45713960+emp-code@users.noreply.github.com>
2020-01-22 09:46:44 +0200

    Make holder unsigned

--- Common/Brotli.c
+++ Common/Brotli.c
@@ -3,12 +3,12 @@
 #include <brotli/encode.h>
 
 __attribute__((warn_unused_result))
-int brotliCompress(char ** const holder, size_t * const lenData) {
+int brotliCompress(unsigned char ** const holder, size_t * const lenData) {
 	size_t lenOut = *lenData;
 	if (lenOut < 100) lenOut += 100; // compressed version can be larger with very small files
-	uint8_t * const output = malloc(lenOut);
+	unsigned char * const output = malloc(lenOut);
 
-	if (BrotliEncoderCompress(BROTLI_MAX_QUALITY, BROTLI_MAX_WINDOW_BITS, BROTLI_DEFAULT_MODE, *lenData, (uint8_t*)(*holder), &lenOut, output) == BROTLI_FALSE) {
+	if (BrotliEncoderCompress(BROTLI_MAX_QUALITY, BROTLI_MAX_WINDOW_BITS, BROTLI_DEFAULT_MODE, *lenData, *holder, &lenOut, output) == BROTLI_FALSE) {
 		free(output);
 		return -1;
 	}
--- Common/Brotli.h
+++ Common/Brotli.h
@@ -1,6 +1,6 @@
 #ifndef AEM_INCLUDES_BROTLI_H
 #define AEM_INCLUDES_BROTLI_H
 
-int brotliCompress(char ** const holder, size_t * const lenData);
+int brotliCompress(unsigned char ** const holder, size_t * const lenData);
 
 #endif
