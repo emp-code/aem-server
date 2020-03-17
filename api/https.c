397f9a60f3e5513498613d27d093ce1c2ba77110
EMPcode <dev@empost.eu>
2020-03-17 20:48:58 +0200

    Cleaner way of checking values

--- api/https.c
+++ api/https.c
@@ -115,25 +115,25 @@ static int getRequestType(const char * const req, size_t lenReq, bool * const ke
 	if (secDest != NULL) {
 		secDest += 18;
 		if (
-		   strncasecmp(secDest, "audio", 5) == 0
-		|| strncasecmp(secDest, "audioworklet", 12) == 0
-		|| strncasecmp(secDest, "document", 8) == 0
-		|| strncasecmp(secDest, "embed", 5) == 0
-		|| strncasecmp(secDest, "font", 4) == 0
-		|| strncasecmp(secDest, "image", 5) == 0
-		|| strncasecmp(secDest, "manifest", 8) == 0
-		|| strncasecmp(secDest, "nested-document", 15) == 0
-		|| strncasecmp(secDest, "object", 6) == 0
-		|| strncasecmp(secDest, "paintworklet", 12) == 0
-		|| strncasecmp(secDest, "report", 6) == 0
-		|| strncasecmp(secDest, "script", 6) == 0
-		|| strncasecmp(secDest, "serviceworker", 13) == 0
-		|| strncasecmp(secDest, "sharedworker", 12) == 0
-		|| strncasecmp(secDest, "style", 5) == 0
-		|| strncasecmp(secDest, "track", 5) == 0
-		|| strncasecmp(secDest, "video", 5) == 0
-		|| strncasecmp(secDest, "worker", 6) == 0
-		|| strncasecmp(secDest, "xslt", 4) == 0
+		   0 == strncasecmp(secDest, "audio", 5)
+		|| 0 == strncasecmp(secDest, "audioworklet", 12)
+		|| 0 == strncasecmp(secDest, "document", 8)
+		|| 0 == strncasecmp(secDest, "embed", 5)
+		|| 0 == strncasecmp(secDest, "font", 4)
+		|| 0 == strncasecmp(secDest, "image", 5)
+		|| 0 == strncasecmp(secDest, "manifest", 8)
+		|| 0 == strncasecmp(secDest, "nested-document", 15)
+		|| 0 == strncasecmp(secDest, "object", 6)
+		|| 0 == strncasecmp(secDest, "paintworklet", 12)
+		|| 0 == strncasecmp(secDest, "report", 6)
+		|| 0 == strncasecmp(secDest, "script", 6)
+		|| 0 == strncasecmp(secDest, "serviceworker", 13)
+		|| 0 == strncasecmp(secDest, "sharedworker", 12)
+		|| 0 == strncasecmp(secDest, "style", 5)
+		|| 0 == strncasecmp(secDest, "track", 5)
+		|| 0 == strncasecmp(secDest, "video", 5)
+		|| 0 == strncasecmp(secDest, "worker", 6)
+		|| 0 == strncasecmp(secDest, "xslt", 4)
 		) return AEM_HTTPS_REQUEST_INVALID;
 	}
 
