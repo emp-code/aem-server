837978b090aac8955ca8b9fae0bf9dd2de63c9a8
EMPcode <dev@empost.eu>
2020-03-20 16:28:39 +0200

    Stricter start-up checks

--- storage/main.c
+++ storage/main.c
@@ -494,8 +494,8 @@ int main(int argc, char *argv[]) {
 	setlocale(LC_ALL, "C");
 	openlog("AEM-Sto", LOG_PID, LOG_MAIL);
 
-	if (argc > 1 || argv == NULL) {syslog(LOG_ERR, "Terminating: Invalid arguments"); return EXIT_FAILURE;}
-	if (getuid()      == 0) {syslog(LOG_ERR, "Terminating: Must not be started as root"); return EXIT_FAILURE;}
+	if (argc != 1 || argv == NULL) {syslog(LOG_ERR, "Terminating: Invalid arguments"); return EXIT_FAILURE;}
+	if (getuid() == 0 || getgid() == 0) {syslog(LOG_ERR, "Terminating: Must not be started as root"); return EXIT_FAILURE;}
 	if (setSignals()  != 0) {syslog(LOG_ERR, "Terminating: Failed setting up signal handling"); return EXIT_FAILURE;}
 	if (sodium_init()  < 0) {syslog(LOG_ERR, "Terminating: Failed initializing libsodium"); return EXIT_FAILURE;}
 
