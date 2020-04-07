ebe901b98dd903aab74774513c80e6db019bc4bb
EMPcode <dev@empost.eu>
2020-04-07 13:54:08 +0300

    Put common TLS setup code in https_setup.c

--- api/https.c
+++ api/https.c
@@ -94,41 +94,7 @@ static bool isRequestValid(const char * const req, const size_t lenReq, bool * c
 	return true;
 }
 
-void tlsFree(void) {
-	mbedtls_ssl_free(&ssl);
-	mbedtls_ssl_config_free(&conf);
-	mbedtls_entropy_free(&entropy);
-	mbedtls_ctr_drbg_free(&ctr_drbg);
-}
-
-int tlsSetup(mbedtls_x509_crt * const tlsCert, mbedtls_pk_context * const tlsKey) {
-	mbedtls_ssl_init(&ssl);
-	mbedtls_ssl_config_init(&conf);
-	mbedtls_entropy_init(&entropy);
-	mbedtls_ctr_drbg_init(&ctr_drbg);
-
-	int ret = mbedtls_ssl_config_defaults(&conf, MBEDTLS_SSL_IS_SERVER, MBEDTLS_SSL_TRANSPORT_STREAM, MBEDTLS_SSL_PRESET_DEFAULT);
-	if (ret != 0) {syslog(LOG_ERR, "mbedtls_ssl_config_defaults failed: %d", ret); return -1;}
-
-	mbedtls_ssl_conf_ca_chain(&conf, tlsCert->next, NULL);
-	mbedtls_ssl_conf_ciphersuites(&conf, https_ciphersuites);
-	mbedtls_ssl_conf_curves(&conf, https_curves);
-	mbedtls_ssl_conf_min_version(&conf, MBEDTLS_SSL_MAJOR_VERSION_3, MBEDTLS_SSL_MINOR_VERSION_3); // Require TLS v1.2+
-	mbedtls_ssl_conf_read_timeout(&conf, AEM_HTTPS_TIMEOUT);
-	mbedtls_ssl_conf_rng(&conf, mbedtls_ctr_drbg_random, &ctr_drbg);
-	mbedtls_ssl_conf_sig_hashes(&conf, https_hashes);
-
-	ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, NULL, 0);
-	if (ret != 0) {syslog(LOG_ERR, "mbedtls_ctr_drbg_seed failed: %d", ret); return -1;}
-
-	ret = mbedtls_ssl_conf_own_cert(&conf, tlsCert, tlsKey);
-	if (ret != 0) {syslog(LOG_ERR, "mbedtls_ssl_conf_own_cert failed: %d", ret); return -1;}
-
-	ret = mbedtls_ssl_setup(&ssl, &conf);
-	if (ret != 0) {syslog(LOG_ERR, "mbedtls_ssl_setup failed: %d", ret); return -1;}
-
-	return 0;
-}
+#include "../Common/https_setup.c"
 
 void respond_https(int sock) {
 	mbedtls_ssl_set_bio(&ssl, &sock, mbedtls_net_send, mbedtls_net_recv, NULL);
--- web/https.c
+++ web/https.c
@@ -62,52 +62,7 @@ void freeHtml(void) {
 	lenHtml = 0;
 }
 
-static int sni(void * const empty, mbedtls_ssl_context * const ssl2, const unsigned char * const hostname, const size_t len) {
-	if (empty != NULL || ssl2 != &ssl) return -1;
-	if (len == 0) return 0;
-
-	return (hostname != NULL && (
-	(len == lenDomain && memcmp(hostname, domain, lenDomain) == 0) ||
-	(len == lenDomain + 8 && memcmp(hostname, "mta-sts.", 8) == 0 && memcmp(hostname + 8, domain, lenDomain) == 0)
-	)) ? 0 : -1;
-}
-
-int tlsSetup(mbedtls_x509_crt * const tlsCert, mbedtls_pk_context * const tlsKey) {
-	mbedtls_ssl_init(&ssl);
-	mbedtls_ssl_config_init(&conf);
-	mbedtls_entropy_init(&entropy);
-	mbedtls_ctr_drbg_init(&ctr_drbg);
-
-	int ret = mbedtls_ssl_config_defaults(&conf, MBEDTLS_SSL_IS_SERVER, MBEDTLS_SSL_TRANSPORT_STREAM, MBEDTLS_SSL_PRESET_DEFAULT);
-	if (ret != 0) {syslog(LOG_ERR, "mbedtls_ssl_config_defaults failed: %d", ret); return -1;}
-
-	mbedtls_ssl_conf_ca_chain(&conf, tlsCert->next, NULL);
-	mbedtls_ssl_conf_ciphersuites(&conf, https_ciphersuites);
-	mbedtls_ssl_conf_curves(&conf, https_curves);
-	mbedtls_ssl_conf_min_version(&conf, MBEDTLS_SSL_MAJOR_VERSION_3, MBEDTLS_SSL_MINOR_VERSION_3); // Require TLS v1.2+
-	mbedtls_ssl_conf_read_timeout(&conf, AEM_HTTPS_TIMEOUT);
-	mbedtls_ssl_conf_rng(&conf, mbedtls_ctr_drbg_random, &ctr_drbg);
-	mbedtls_ssl_conf_sig_hashes(&conf, https_hashes);
-	mbedtls_ssl_conf_sni(&conf, sni, NULL);
-
-	ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, NULL, 0);
-	if (ret != 0) {syslog(LOG_ERR, "mbedtls_ctr_drbg_seed failed: %d", ret); return -1;}
-
-	ret = mbedtls_ssl_conf_own_cert(&conf, tlsCert, tlsKey);
-	if (ret != 0) {syslog(LOG_ERR, "mbedtls_ssl_conf_own_cert failed: %d", ret); return -1;}
-
-	ret = mbedtls_ssl_setup(&ssl, &conf);
-	if (ret != 0) {syslog(LOG_ERR, "mbedtls_ssl_setup failed: %d", ret); return -1;}
-
-	return 0;
-}
-
-void tlsFree(void) {
-	mbedtls_ssl_free(&ssl);
-	mbedtls_ssl_config_free(&conf);
-	mbedtls_entropy_free(&entropy);
-	mbedtls_ctr_drbg_free(&ctr_drbg);
-}
+#include "../Common/https_setup.c"
 
 static void respond_mtasts(void) {
 	char data[377 + lenDomain];
