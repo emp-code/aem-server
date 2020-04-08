444ddba17ede3a2a65053b8a0297fc0c36989a9f
EMPcode <dev@empost.eu>
2020-04-08 11:06:27 +0300

    Check peer uid/gid on Unix sockets

--- api/post.c
+++ api/post.c
@@ -65,6 +65,14 @@ static void clearDecrypted() {
 	sodium_mprotect_noaccess(decrypted);
 }
 
+static bool peerOk(const int sock) {
+	// TODO: Verify peer PID (get Account/Storage PID from Manager at startup)
+	struct ucred peer;
+	socklen_t lenUc = sizeof(struct ucred);
+	if (getsockopt(sock, SOL_SOCKET, SO_PEERCRED, &peer, &lenUc) == -1) return false;
+	return (peer.gid == getgid() && peer.uid == getuid());
+}
+
 static int makeUnixSocket(const char * const path) {
 	const int sock = socket(AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC, 0);
 	if (sock < 0) return -1;
@@ -74,6 +82,13 @@ static int makeUnixSocket(const char * const path) {
 	strcpy(sa.sun_path, path);
 
 	if (connect(sock, (struct sockaddr*)&sa, strlen(sa.sun_path) + sizeof(sa.sun_family)) == -1) {
+		syslog(LOG_WARNING, "Failed connecting to Unix socket");
+		close(sock);
+		return -1;
+	}
+
+	if (!peerOk(sock)) {
+		syslog(LOG_WARNING, "Invalid Unix socket peer");
 		close(sock);
 		return -1;
 	}
