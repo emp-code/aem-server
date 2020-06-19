2b657e5d2eb2b59a545ce84448979b6a2f83c637
EMPcode <dev@empost.eu>
2020-06-19 09:46:40 +0300

    Remove getBit()

--- enquiry/bit.c
+++ enquiry/bit.c
@@ -1,12 +1,4 @@
-// bit.c: Get or set a specific bit in a byte
 #include "bit.h"
-
-// Get bit #1-8 from a byte
-int getBit(const unsigned char * const byte, const int bit) {
-	const unsigned char tmp = *(byte + ((bit - 1) / 8));
-	const unsigned char mask = 1 << (8 - bit);
-	return (tmp & mask) > 0;
-}
 
 // Set bit #1-8 on a byte
 void setBit(unsigned char * const byte, const int bit, const int value) {
--- enquiry/bit.h
+++ enquiry/bit.h
@@ -1,7 +1,6 @@
 #ifndef AEM_BIT_H
 #define AEM_BIT_H
 
-int getBit(const unsigned char * const byte, const int bit);
 void setBit(unsigned char * const byte, const int bit, const int value);
 
 #endif
