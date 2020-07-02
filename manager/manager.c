6d8514a7db37165276a538bd414c426bc9c98bad
EMPcode <dev@empost.eu>
2020-07-02 13:39:00 +0300

    rlimit nice values

--- manager/manager.c
+++ manager/manager.c
@@ -627,7 +627,9 @@ static int setSubLimits(const int type) {
 
 	if (setrlimit(RLIMIT_OFILE, &rlim) != 0) return -1;
 
-	return 0;
+	rlim.rlim_cur = (typeNice[type] * -1) + 20; // The actual ceiling for the nice value is calculated as 20 - rlim_cur
+	rlim.rlim_max = (typeNice[type] * -1) + 20;
+	return setrlimit(RLIMIT_NICE, &rlim);
 }
 
 __attribute__((warn_unused_result))
@@ -665,9 +667,9 @@ static int process_new(void *params) {
 
 	if (mount(NULL, "/", NULL, MS_PRIVATE | MS_REC, "") != 0) {syslog(LOG_ERR, "Failed private mount"); exit(EXIT_FAILURE);} // With CLONE_NEWNS, prevent propagation of mount events to other mount namespaces
 	if (prctl(PR_SET_PDEATHSIG, SIGUSR2, 0, 0, 0) != 0) {syslog(LOG_ERR, "Failed prctl()"); exit(EXIT_FAILURE);}
-	if (setpriority(PRIO_PROCESS, 0, typeNice[type]) != 0) {syslog(LOG_ERR, "Failed setpriority()"); exit(EXIT_FAILURE);}
 	if (createMount(type) != 0) {syslog(LOG_ERR, "Failed createMount()"); exit(EXIT_FAILURE);}
 	if (setSubLimits(type) != 0) {syslog(LOG_ERR, "Failed setSubLimits()"); exit(EXIT_FAILURE);}
+	if (setpriority(PRIO_PROCESS, 0, typeNice[type]) != 0) {syslog(LOG_ERR, "Failed setpriority()"); exit(EXIT_FAILURE);}
 	if (dropRoot() != 0) {syslog(LOG_ERR, "Failed dropRoot()"); exit(EXIT_FAILURE);}
 	if (setCaps(type) != 0) {syslog(LOG_ERR, "Failed setCaps()"); exit(EXIT_FAILURE);}
 
