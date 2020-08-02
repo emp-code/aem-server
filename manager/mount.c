ac15e3f99502c35e8f789ff59eea3c01e2c9c180
EMPcode <dev@empost.eu>
2020-08-02 11:49:44 +0300

    Order flags

--- manager/mount.c
+++ manager/mount.c
@@ -43,7 +43,7 @@ static int bindMount(const char * const source, const char * const target, const
 	|| mount("",     target, "",   MS_UNBINDABLE, "") != 0
 	) return -1;
 
-	unsigned long mountFlags = MS_BIND | MS_REMOUNT | MS_NOSUID | MS_NODEV | MS_NOATIME | MS_SILENT | MS_NOEXEC;
+	unsigned long mountFlags = MS_BIND | MS_REMOUNT | MS_NOATIME | MS_NODEV | MS_NOEXEC | MS_NOSUID | MS_SILENT;
 
 	if (flags & AEM_MOUNT_RDONLY)
 		mountFlags |= MS_RDONLY;
