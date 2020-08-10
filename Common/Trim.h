d757ac82c5d5f3b97e3c4e08501c86e7e4877a46
EMPcode <dev@empost.eu>
2020-08-10 09:00:12 +0300

    Match function order to .c

--- Common/Trim.h
+++ Common/Trim.h
@@ -3,9 +3,9 @@
 
 void removeControlChars(unsigned char * const text, size_t * const len);
 void convertNbsp(char * const text, size_t * const len);
+void trimSpace(char * const text, size_t * const len);
 void removeSpaceEnd(char * const text, size_t * const len);
 void removeSpaceBegin(char * const text, size_t * const len);
-void trimSpace(char * const text, size_t * const len);
 void trimLinebreaks(char * const text, size_t * const len);
 void trimEnd(const char * const text, size_t * const len);
 
