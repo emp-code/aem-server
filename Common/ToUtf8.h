6acaa9a014b442dd2156049703ca861230524c15
EMPcode <dev@empost.eu>
2020-08-11 16:11:06 +0300

    Add bool header

--- Common/ToUtf8.h
+++ Common/ToUtf8.h
@@ -1,6 +1,8 @@
 #ifndef AEM_INCLUDES_TOUTF8_H
 #define AEM_INCLUDES_TOUTF8_H
 
+#include <stdbool.h>
+
 bool isUtf8(const char * const charset, const size_t len);
 char *toUtf8(const char * const input, const size_t lenInput, int * const lenOut, const char * const charset);
 
