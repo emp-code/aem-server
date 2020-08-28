8b22b679f8731dfd210ce94ff87c3c607742e3ac
EMPcode <dev@empost.eu>
2020-08-28 12:00:48 +0300

    Add const

--- api-oni/http.c
+++ api-oni/http.c
@@ -96,7 +96,7 @@ void respondClient(const int sock) {
 
 		// Request is valid
 		const size_t lenBox = clen - AEM_API_SEALBOX_SIZE;
-		unsigned char *box = malloc(lenBox);
+		unsigned char * const box = malloc(lenBox);
 		if (box == NULL) {syslog(LOG_ERR, "Failed malloc()"); break;}
 
 		if (lenPost > AEM_API_SEALBOX_SIZE) {
