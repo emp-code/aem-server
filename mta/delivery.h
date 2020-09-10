67b757c0db2733a7f3de4e959f1a375fe81d2286
EMPcode <dev@empost.eu>
2020-09-10 16:26:46 +0300

    Cleanup

--- mta/delivery.h
+++ mta/delivery.h
@@ -1,13 +1,9 @@
-#ifndef AEM_DELIVERY_H
-#define AEM_DELIVERY_H
+#ifndef AEM_MTA_DELIVERY_H
+#define AEM_MTA_DELIVERY_H
 
-#include "../Global.h"
 #include "Email.h"
 
 void setSignKey(const unsigned char * const seed);
-void setAccountPid(const pid_t pid);
-void setStoragePid(const pid_t pid);
-
 void deliverMessage(const char * const to, const size_t lenToTotal, const unsigned char * const msgBody, size_t lenMsgBody, struct emailInfo * const email);
 
 #endif
