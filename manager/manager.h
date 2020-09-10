62e330684200e9ff60174883b7bf9574f16f3b25
EMPcode <dev@empost.eu>
2020-09-10 15:45:41 +0300

    Add missing param

--- manager/manager.h
+++ manager/manager.h
@@ -1,8 +1,7 @@
 #ifndef AEM_MANAGER_H
 #define AEM_MANAGER_H
 
-void killAll();
-
+void killAll(int sig);
 void setMasterKey(const unsigned char newKey[crypto_secretbox_KEYBYTES]);
 int loadFiles(void);
 int receiveConnections(void);
