afab88297fb919f908eed17d01aa2faeae801e4d
EMPcode <dev@empost.eu>
2020-10-09 23:31:11 +0300

    isMx param name

--- enquiry/DNS_protocol.h
+++ enquiry/DNS_protocol.h
@@ -1,7 +1,7 @@
 #ifndef AEM_DNS_PROTOCOL_H
 #define AEM_DNS_PROTOCOL_H
 
-int dnsCreateRequest(unsigned char * const rq, const unsigned char * const domain, const size_t lenDomain, const bool mx);
+int dnsCreateRequest(unsigned char * const rq, const unsigned char * const domain, const size_t lenDomain, const bool isMx);
 int dnsResponse_GetMx(const unsigned char * const res, const int resLen, unsigned char * const mxDomain, int * const lenMxDomain);
 uint32_t dnsResponse_GetIp(const unsigned char * const res, const int resLen);
 
