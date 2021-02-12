4647333cdca8a7b315d844c8e738f3d37c9be878
emp-code <dev@empost.eu>
2021-02-12 21:17:50 +0200

    Lower max exec size to 256k

--- Global.h
+++ Global.h
@@ -86,7 +86,7 @@ enum aem_internal_enquiry {
 #define AEM_TIMEOUT_MANAGER_RCV 3
 #define AEM_TIMEOUT_MANAGER_SND 3
 
-#define AEM_MAXSIZE_EXEC 524288 // 512 KiB
+#define AEM_MAXSIZE_EXEC 262144 // 256 KiB
 
 enum aem_api_commands {
 	AEM_API_ACCOUNT_BROWSE,
