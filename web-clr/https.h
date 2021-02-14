0f0573acfeacb8b09bc4909a264d911d5bd1b5c5
emp-code <dev@empost.eu>
2021-02-14 08:24:05 +0200

    Cleanup

--- web-clr/https.h
+++ web-clr/https.h
@@ -1,9 +1,6 @@
 #ifndef AEM_WEB_HTTPS_H
 #define AEM_WEB_HTTPS_H
 
-int setHtml(const unsigned char * const data, const size_t len);
-void freeHtml(void);
-
 int tlsSetup(void);
 void tlsFree(void);
 
