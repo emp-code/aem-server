362afbdea3db0b9f85717cd4b239eea0a670ee0d
emp-code <dev@empost.eu>
2021-03-12 16:42:21 +0200

    Allow header at beginning

--- mta/dkim.c
+++ mta/dkim.c
@@ -165,9 +165,9 @@ static bool verifyDkimSig(mbedtls_pk_context * const pk, const unsigned char * c
 	char *h = strtok(copyHeaders, ":");
 	while (h != NULL) {
 		const size_t lenH = strlen(h);
-		const unsigned char *s = (unsigned char*)strcasestr(headers + 1, h);
+		const unsigned char *s = (unsigned char*)strcasestr(headers, h);
 
-		while (s != NULL && (*(s - 1) != '\n' || s[lenH] != ':')) {
+		while (s != NULL && (((char*)s != headers && *(s - 1) != '\n') || s[lenH] != ':')) {
 			s = (unsigned char*)strcasestr((char*)s + lenH, h);
 		}
 
