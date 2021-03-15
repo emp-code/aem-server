fd1df92c864e2d40375f65f6b99b8f5083c1513c
emp-code <dev@empost.eu>
2021-03-15 13:18:18 +0200

    Allow plus signs in outgoing email addresses

--- Common/ValidEmail.c
+++ Common/ValidEmail.c
@@ -14,7 +14,7 @@ bool isValidEmail(const char * const email) {
 
 	for (;lenLocal < lenEmail; lenLocal++) {
 		if (isalnum(email[lenLocal])) continue;
-		if (lenLocal > 0 && (email[lenLocal] == '-' || email[lenLocal] == '_' || (email[lenLocal] == '.' && email[lenLocal - 1] != '.'))) continue;
+		if (lenLocal > 0 && (email[lenLocal] == '-' || email[lenLocal] == '+' || email[lenLocal] == '_' || (email[lenLocal] == '.' && email[lenLocal - 1] != '.'))) continue;
 		if (email[lenLocal] == '@') break;
 		return false;
 	}
