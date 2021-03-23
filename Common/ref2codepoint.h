1c6a4e9f6a58d04a7620384b94617ef4e474610a
emp-code <dev@empost.eu>
2021-03-23 03:28:23 +0200

    Cleaner ref2codepoint

--- Common/ref2codepoint.h
+++ Common/ref2codepoint.h
@@ -2,6 +2,6 @@
 #define AEM_REF2CODEPOINT_H
 
 unsigned int ref2codepoint(const unsigned char * const ref);
-unsigned int ref2codepoint2(const unsigned char * const ref, unsigned int * const one, unsigned int * const two);
+void ref2codepoint2(const unsigned char * const ref, unsigned int * const one, unsigned int * const two);
 
 #endif
