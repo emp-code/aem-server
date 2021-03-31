edfa3d84d5e5a0072497b91bcbd2e6617eb72d13
emp-code <dev@empost.eu>
2021-03-31 20:51:39 +0300

    Verify end is not null

--- mta/date.c
+++ mta/date.c
@@ -37,8 +37,8 @@ time_t smtp_getTime(const char *b, unsigned char * const tzp) {
 	) ? 4 : 0;
 
 	while (b[offset] == ' ') offset++;
-
 	if (!isdigit(b[offset])) return 0;
+
 	char *end = NULL;
 	const long mday = strtol(b + offset, &end, 10);
 	if (mday < 1 || mday > 31 || end == NULL) return 0;
@@ -55,7 +55,7 @@ time_t smtp_getTime(const char *b, unsigned char * const tzp) {
 
 	long year = strtol(b + offset, &end, 10);
 	if (year > 20 && year < 100) year += 2000;
-	if (year < 2020) return 0;
+	if (year < 2020 || end == NULL) return 0;
 
 	b = end;
 	offset = 1;
@@ -63,22 +63,24 @@ time_t smtp_getTime(const char *b, unsigned char * const tzp) {
 
 	const long hour = strtol(b + offset, &end, 10);
 	if (hour < 0 || hour > 23) return 0;
-	if (*end != ':') return 0;
+	if (end == NULL || *end != ':') return 0;
+
 	b = end;
 	offset = 1;
 	while (b[offset] == ' ') offset++;
 
 	const long min = strtol(b + offset, &end, 10);
 	if (min < 0 || min > 59) return 0;
-	if (*end != ':') return 0;
+	if (end == NULL || *end != ':') return 0;
+
 	b = end;
 	offset = 1;
-
 	while (b[offset] == ' ') offset++;
 
 	const long sec = strtol(b + offset, &end, 10);
 	if (sec < 0 || sec > 59) return 0;
-	if (*end != ' ') return 0;
+	if (end == NULL || *end != ' ') return 0;
+
 	b = end;
 	offset = 0;
 	while (b[offset] == ' ') offset++;
