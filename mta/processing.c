8573f9d710bb3203abdcfb8b61348a2d30ffdbac
emp-code <dev@empost.eu>
2021-10-20 05:58:11 +0300

    Set length limit to final filename

--- mta/processing.c
+++ mta/processing.c
@@ -449,12 +449,14 @@ static unsigned char *decodeMp(const unsigned char * const src, size_t *outLen, 
 			if (fn == NULL || lenFn < 1) {
 				fn = (char[]){'A','E','M'}; // TODO, name based on message/sender/etc
 				lenFn = 3;
-			} else if (lenFn > 256) lenFn = 256;
+			} else {
+				char fn2[lenFn];
+				memcpy(fn2, fn, lenFn);
+				decodeEncodedWord((unsigned char*)fn2, &lenFn);
+				fn = fn2;
 
-			char fn2[lenFn];
-			memcpy(fn2, fn, lenFn);
-			decodeEncodedWord((unsigned char*)fn2, &lenFn);
-			fn = fn2;
+				if (lenFn > 256) lenFn = 256;
+			}
 
 			if (17 + lenFn + lenNew <= AEM_API_BOX_SIZE_MAX) {
 				email->attachment[email->attachCount] = malloc(17 + lenFn + lenNew);
