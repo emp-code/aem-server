766a8b4f80bf0443c983c34961d8a2d77e984240
emp-code <dev@empost.eu>
2021-10-22 05:42:16 +0300

    Remove support for messages under minimum size

--- Common/Message.c
+++ Common/Message.c
@@ -6,6 +6,7 @@ static uint8_t msg_getPadAmount(const size_t lenContent) {
 static unsigned char *msg_encrypt(const unsigned char * const pk, const unsigned char * const content, const size_t lenContent, size_t * const lenEncrypted) {
 	const uint8_t padAmount = msg_getPadAmount(lenContent);
 	const size_t lenPadded = lenContent + padAmount + crypto_sign_BYTES;
+	if (lenPadded < AEM_MSG_MINSIZE_DEC) return NULL;
 
 	unsigned char * const clear = sodium_malloc((lenPadded < AEM_MSG_MINSIZE_DEC) ? AEM_MSG_MINSIZE_DEC : lenPadded);
 	if (clear == NULL) {syslog(LOG_ERR, "Failed allocation"); return NULL;}
@@ -14,19 +15,11 @@ static unsigned char *msg_encrypt(const unsigned char * const pk, const unsigned
 	randombytes_buf_deterministic(clear + lenContent, padAmount, clear); // First randombytes_SEEDBYTES of message determine the padding bytes
 	crypto_sign_detached(clear + lenPadded - crypto_sign_BYTES, NULL, clear, lenPadded - crypto_sign_BYTES, sign_skey);
 
-	size_t lenDec;
-	if (lenPadded < AEM_MSG_MINSIZE_DEC) {
-		const int lenExtraPad = AEM_MSG_MINSIZE_DEC - lenPadded - 1;
-		randombytes_buf_deterministic(clear + lenPadded, lenExtraPad, clear + lenPadded - crypto_sign_BYTES); // First randombytes_SEEDBYTES of signature determine the padding bytes
-		clear[AEM_MSG_MINSIZE_DEC - 1] = lenExtraPad; // Last byte stores extra-pad amount
-		lenDec = AEM_MSG_MINSIZE_DEC;
-	} else lenDec = lenPadded;
-
-	*lenEncrypted = lenDec + crypto_box_SEALBYTES;
+	*lenEncrypted = lenPadded + crypto_box_SEALBYTES;
 	unsigned char *encrypted = malloc(*lenEncrypted);
 	if (encrypted == NULL) {syslog(LOG_ERR, "Failed allocation"); sodium_free(clear); return NULL;}
 
-	const int ret = crypto_box_seal(encrypted, clear, lenDec, pk);
+	const int ret = crypto_box_seal(encrypted, clear, lenPadded, pk);
 	sodium_free(clear);
 
 	if (ret != 0) {
