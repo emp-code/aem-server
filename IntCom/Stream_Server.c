67088ef0cc28089e7bac7fdddfbe8ab378321e4a
emp-code <dev@empost.eu>
2023-02-05 10:21:35 +0200

    Account for encryption overhead

--- IntCom/Stream_Server.c
+++ IntCom/Stream_Server.c
@@ -102,7 +102,7 @@ void intcom_serve_stream(void) {
 				break;
 			}
 
-			if (lenEnc == SIZE_MAX || lenDec + lenEnc > AEM_SMTP_MAX_SIZE_BODY) { // Finished
+			if (lenEnc == SIZE_MAX || lenDec + (lenEnc - crypto_secretstream_xchacha20poly1305_ABYTES) > AEM_SMTP_MAX_SIZE_BODY) { // Finished
 				crypto_secretstream_xchacha20poly1305_rekey(&ss_state);
 				break;
 			}
