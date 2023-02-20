6b70da1f0544778964fafd48936c0a69e33e4ff5
emp-code <dev@empost.eu>
2023-02-20 09:36:44 +0200

    Update CET characters

--- Global.h
+++ Global.h
@@ -69,12 +69,17 @@ enum aem_internal_enquiry {
 #define AEM_FLAG_NEWER 1
 
 // Control-Enriched Text (CET)
+#define AEM_CET_CHAR_BIG 0x01 // Big text
+#define AEM_CET_CHAR_SML 0x02 // Small text
+#define AEM_CET_CHAR_SUB 0x03 // Subscript
+#define AEM_CET_CHAR_SUP 0x04 // Superscript
+#define AEM_CET_CHAR_MNO 0x05 // Monospace
 #define AEM_CET_CHAR_BLD 0x06 // Bold
 #define AEM_CET_CHAR_ITA 0x07 // Italics
 #define AEM_CET_CHAR_UNL 0x08 // Underline
 #define AEM_CET_CHAR_STR 0x09 // Strikethrough
-// 0x0A = LF
-// 0x0B unused
+//                       0x0A == <br>
+#define AEM_CET_CHAR_HRL 0x0B // <hr>
 #define AEM_CET_CHAR_LNK 0x0C // a, frame, iframe (+1 = HTTPS)
 #define AEM_CET_CHAR_FIL 0x0E // img, audio, video, source, object, embed (+1 = HTTPS)
 #define AEM_CET_CHAR_SEP 0x7F // Separator (internal use)
