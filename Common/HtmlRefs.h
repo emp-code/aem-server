21c6eb405f3ceb1b9c3feb4ede06a3dbf9619b2b
emp-code <dev@empost.eu>
2023-03-07 17:33:46 +0200

    Correct param name

--- Common/HtmlRefs.h
+++ Common/HtmlRefs.h
@@ -1,6 +1,6 @@
 #ifndef AEM_HTMLREFS_H
 #define AEM_HTMLREFS_H
 
-int getHtmlCharacter(unsigned char * const src, const size_t lenSrc, const size_t posStart, size_t * const posWrite);
+int getHtmlCharacter(unsigned char * const src, const size_t lenSrc, const size_t posInput, size_t * const lenOut);
 
 #endif
