8b442b78922a0d020c093983c1d90000eb792a32
emp-code <dev@empost.eu>
2023-06-15 15:00:28 +0300

    Verify/close socket errors

--- Common/CreateSocket.c
+++ Common/CreateSocket.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <strings.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 #include "../Config.h"
 #include "../Common/memeq.h"
@@ -26,6 +27,7 @@ static int setSocketTimeout(const int sock, const time_t rcvSec, const time_t sn
 __attribute__((warn_unused_result))
 int createSocket(const bool loopback, const time_t rcvTimeout, const time_t sndTimeout) {
 	const int sock = socket(AF_INET, SOCK_STREAM | SOCK_CLOEXEC, 0);
+	if (sock < 0) return -1;
 
 	struct sockaddr_in servAddr;
 	bzero((char*)&servAddr, sizeof(servAddr));
@@ -34,29 +36,32 @@ int createSocket(const bool loopback, const time_t rcvTimeout, const time_t sndT
 
 	const int intTrue = 1;
 #ifndef AEM_MANAGER
-	if (setsockopt(sock, SOL_SOCKET, SO_REUSEPORT,   (const void*)&intTrue, sizeof(int)) != 0) return -1;
+	if (setsockopt(sock, SOL_SOCKET, SO_REUSEPORT,   (const void*)&intTrue, sizeof(int)) != 0) {close(sock); return -1;}
 #endif
-	if (setsockopt(sock, SOL_SOCKET, SO_LOCK_FILTER, (const void*)&intTrue, sizeof(int)) != 0) return -1;
+	if (setsockopt(sock, SOL_SOCKET, SO_LOCK_FILTER, (const void*)&intTrue, sizeof(int)) != 0) {close(sock); return -1;}
 
 	if (loopback) {
 		servAddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-		if (setsockopt(sock, SOL_SOCKET, SO_BINDTODEVICE, "lo", 3) != 0) return -1;
-		if (setsockopt(sock, SOL_SOCKET, SO_DONTROUTE, (const void*)&intTrue, sizeof(int)) != 0) return -1;
+		if (setsockopt(sock, SOL_SOCKET, SO_BINDTODEVICE, "lo", 3) != 0) {close(sock); return -1;}
+		if (setsockopt(sock, SOL_SOCKET, SO_DONTROUTE, (const void*)&intTrue, sizeof(int)) != 0) {close(sock); return -1;}
 	} else {
 		servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 		struct if_nameindex * const ni = if_nameindex();
 		for (int i = 0;; i++) {
-			if (ni[i].if_index == 0) {if_freenameindex(ni); return -1;}
+			if (ni[i].if_index == 0) {if_freenameindex(ni); close(sock); return -1;}
 			if (memeq(ni[i].if_name, "lo", 2)) continue;
-			if (setsockopt(sock, SOL_SOCKET, SO_BINDTODEVICE, ni[i].if_name, strlen(ni[i].if_name) + 1) != 0) {if_freenameindex(ni); return -1;}
+			if (setsockopt(sock, SOL_SOCKET, SO_BINDTODEVICE, ni[i].if_name, strlen(ni[i].if_name) + 1) != 0) {if_freenameindex(ni); close(sock); return -1;}
 			break;
 		}
 		if_freenameindex(ni);
 	}
 
-	return (
+	if (
 	   setSocketTimeout(sock, rcvTimeout, sndTimeout) == 0
 	&& bind(sock, (struct sockaddr*)&servAddr, sizeof(servAddr)) == 0
 	&& listen(sock, AEM_BACKLOG) == 0
-	) ? sock : -1;
+	) return sock;
+
+	close(sock);
+	return -1;
 }
