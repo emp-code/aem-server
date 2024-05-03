4242f496386642d97b633185624572e6da55b4e3
emp-code <dev@empost.eu>
2024-05-03 22:07:54 +0300

    Remove isRequestValid

--- api/isRequestValid.h
+++
@@ -1,8 +0,0 @@
-#ifndef AEM_API_ISREQUESTVALID_H
-#define AEM_API_ISREQUESTVALID_H
-
-#include <stdbool.h>
-
-bool isRequestValid(const char * const req, const size_t lenReq, bool * const keepAlive, long * const clen);
-
-#endif
