c53bd21b7cd824373251220461a619ec43a09e27
emp-code <dev@empost.eu>
2024-05-29 06:28:15 +0300

    Removed function

--- deliver/format.h
+++ deliver/format.h
@@ -6,7 +6,6 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-void makeAttachment(unsigned char * const att, const size_t lenAtt, const uint32_t ts, const unsigned char parentId[16]);
 unsigned char *makeExtMsg(struct emailInfo * const email, size_t * const lenOut, const bool allVer);
 
 #endif
