8876c7f29d783c3e0a15fb605b3ad41320aea614
emp-code <dev@empost.eu>
2024-07-16 03:30:58 +0300

    Replace mbedTLS header with wolfSSL

--- mta/cert.h
+++ mta/cert.h
@@ -1,7 +1,7 @@
 #ifndef AEM_CERT_H
 #define AEM_CERT_H
 
-#include <mbedtls/ssl.h>
+#include <wolfssl/ssl.h>
 
 //uint8_t cert_getTlsInfo_type(const mbedtls_x509_crt * const cert);
 uint8_t cert_getTlsInfo_name(WOLFSSL_X509 * const cert, const unsigned char * const greet, const size_t lenGreet, unsigned char *envFr, size_t lenEnvFr, unsigned char *hdrFr, size_t lenHdrFr);
