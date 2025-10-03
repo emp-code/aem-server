2a136a9edd5cfb721d4dc27513a1d7e6dd33fe1f
emp-code <dev@empost.eu>
2025-10-03 20:33:38 +0300

    Set userhash to exactly double the length of the final sig

--- Common/Signature.c
+++ Common/Signature.c
@@ -17,18 +17,18 @@ void delSigKey(void) {
 void aem_sign_message(unsigned char * const msg, const size_t lenMsg, const unsigned char usk[AEM_USK_KEYLEN]) {
 	if (lenMsg <= AEM_MSG_HDR_SZ) return;
 
-	unsigned char uHash[54];
-	crypto_generichash((unsigned char*)uHash, 54, msg + 26, lenMsg - 26, usk, AEM_USK_KEYLEN); // Skip 26*8=208 of the 212 signature bits (remaining four bits are zero)
+	unsigned char uHash[53];
+	crypto_generichash((unsigned char*)uHash, 53, msg + 26, lenMsg - 26, usk, AEM_USK_KEYLEN); // Skip 26*8=208 of the 212 signature bits (remaining four bits are zero)
 	unsigned char sHash[27];
-	crypto_generichash((unsigned char*)sHash, 27, uHash,    56,          ssk, AEM_SSK_KEYLEN);
+	crypto_generichash((unsigned char*)sHash, 27, uHash,    53,          ssk, AEM_SSK_KEYLEN);
 
 	memcpy(msg, sHash, 26);
 	msg[26] = (sHash[26] & 15) | (msg[26] & 240); // Copy the remaining four Signature bits, but preserve the two Type bits and two BinTs bits
 }
 
-bool aem_sig_verify(const unsigned char uHash[54], const unsigned char sHash_test[27]) {
+bool aem_sig_verify(const unsigned char uHash[53], const unsigned char sHash_test[27]) {
 	unsigned char sHash_real[27];
-	crypto_generichash((unsigned char*)sHash_real, 27, uHash, 54, ssk, AEM_SSK_KEYLEN);
+	crypto_generichash((unsigned char*)sHash_real, 27, uHash, 53, ssk, AEM_SSK_KEYLEN);
 	sHash_real[26] &= 15;
 	return (sodium_memcmp(sHash_real, sHash_test, 27) == 0);
 }
