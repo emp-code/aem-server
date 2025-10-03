df3d13281fc677a9064b96062e74812f6022070e
emp-code <dev@empost.eu>
2025-10-03 21:18:27 +0300

    Correct uhash length

--- Common/Signature.h
+++ Common/Signature.h
@@ -8,6 +8,6 @@ void setSigKey(const unsigned char baseKey[AEM_KDF_SUB_KEYLEN]);
 void delSigKey(void);
 
 void aem_sign_message(unsigned char * const msg, const size_t lenMsg, const unsigned char usk[AEM_USK_KEYLEN]);
-bool aem_sig_verify(const unsigned char uHash[54], const unsigned char sHash_test[27]);
+bool aem_sig_verify(const unsigned char uHash[53], const unsigned char sHash_test[27]);
 
 #endif
