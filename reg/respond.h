17d440fe93b18057c8a237f5b7795d09864f8933
emp-code <dev@empost.eu>
2025-10-03 13:41:06 +0300

    Remove unused

--- reg/respond.h
+++ reg/respond.h
@@ -1,11 +1,6 @@
 #ifndef AEM_REG_RESPOND_H
 #define AEM_REG_RESPOND_H
 
-#include "../Common/AEM_KDF.h"
-
-void setRegKey(const unsigned char baseKey[AEM_KDF_SUB_KEYLEN]);
-void delRegKey(void);
-
 #ifdef AEM_TLS
 bool
 #else
