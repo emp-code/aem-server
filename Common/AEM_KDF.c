5c8627943b4ee2c20aad361c76f0c25b7952f626
emp-code <dev@empost.eu>
2025-10-24 01:14:41 +0300

    Move aem_getUserId next to the related UAK function

--- Common/AEM_KDF.c
+++ Common/AEM_KDF.c
@@ -24,6 +24,14 @@ void aem_kdf_uak(unsigned char * const out, const size_t lenOut, const uint64_t 
 	key);
 }
 
+// Get UserID from UAK
+__attribute__((warn_unused_result))
+uint16_t aem_getUserId(const unsigned char uak[AEM_KDF_UAK_KEYLEN]) {
+	uint16_t uid;
+	aem_kdf_uak((unsigned char*)&uid, sizeof(uint16_t), 0, false, 0, uak);
+	return uid & 4095;
+}
+
 // Use the 320-bit subkey with a 56-bit nonce to generate up to 16 KiB
 __attribute__((nonnull))
 void aem_kdf_sub(unsigned char * const out, const size_t lenOut, const uint64_t n, const unsigned char key[AEM_KDF_SUB_KEYLEN]) {
@@ -32,13 +40,6 @@ void aem_kdf_sub(unsigned char * const out, const size_t lenOut, const uint64_t 
 	/* Nonce */ (const uint8_t[]){key[32], key[33], key[34], key[35], key[36], key[37], key[38], key[39], ((const uint8_t*)&n)[0], ((const uint8_t*)&n)[1], ((const uint8_t*)&n)[2], ((const uint8_t*)&n)[3]},
 	/* Counter */ (((const uint8_t*)&n)[4] << 8) | (((const uint8_t*)&n)[5] << 16) | ((unsigned int)(((const uint8_t*)&n)[6]) << 24),
 	key);
-}
-
-// Get UserID from UAK
-uint16_t aem_getUserId(const unsigned char uak[AEM_KDF_UAK_KEYLEN]) {
-	uint16_t uid;
-	aem_kdf_uak((unsigned char*)&uid, sizeof(uint16_t), 0, false, 0, uak);
-	return uid & 4095;
 }
 
 #ifdef AEM_KDF_UMK
