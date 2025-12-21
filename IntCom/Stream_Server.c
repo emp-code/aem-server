db81f94e720f9e3ffd716d661b93a0b8c2f4afc6
emp-code <dev@empost.eu>
2025-12-21 11:51:44 +0200

    Use AEM_FD_SOCK_MAIN and AEM_FD_SOCK_CLIENT

--- IntCom/Stream_Server.c
+++ IntCom/Stream_Server.c
@@ -27,8 +27,6 @@ struct dlvEmail {
 static unsigned char intcom_key[crypto_secretstream_xchacha20poly1305_KEYBYTES];
 
 static volatile sig_atomic_t terminate = 0;
-int sockListen = -1;
-int sockClient = -1;
 
 void sigTerm(const int s) {
 	terminate = 1;
@@ -49,27 +47,26 @@ static int bindSocket(const int sock) {
 void intcom_serve_stream(void) {
 	if (sodium_is_zero(intcom_key, crypto_secretstream_xchacha20poly1305_KEYBYTES)) return;
 
-	sockListen = socket(AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC, 0);
-	if (bindSocket(sockListen) != 0) {syslog(LOG_ERR, "Failed bindSocket(): %m"); return;}
-	listen(sockListen, 50);
+	if (socket(AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC, 0) != AEM_FD_SOCK_MAIN) {syslog(LOG_ERR, "Failed creating socket: %m"); return;}
+	if (bindSocket(AEM_FD_SOCK_MAIN) != 0) {syslog(LOG_ERR, "Failed bindSocket(): %m"); return;}
+	listen(AEM_FD_SOCK_MAIN, 50);
 
 	struct dlvEmail *dlv = malloc(sizeof(struct dlvEmail));
 	if (dlv == NULL) {syslog(LOG_ERR, "Failed allocation"); return;}
 
 	while (terminate == 0) {
-		sockClient = accept4(sockListen, NULL, NULL, SOCK_CLOEXEC);
-		if (sockClient < 0) continue;
+		if (accept4(AEM_FD_SOCK_MAIN, NULL, NULL, SOCK_CLOEXEC) != AEM_FD_SOCK_CLIENT) continue;
 
-		if (!peerOk(sockClient)) {
+		if (!peerOk(AEM_FD_SOCK_CLIENT)) {
 			syslog(LOG_WARNING, "Connection rejected from invalid peer");
-			close(sockClient);
+			close(AEM_FD_SOCK_CLIENT);
 			continue;
 		}
 
 		crypto_secretstream_xchacha20poly1305_state ss_state;
 		unsigned char ss_header[crypto_secretstream_xchacha20poly1305_HEADERBYTES];
-		if (recv(sockClient, ss_header, crypto_secretstream_xchacha20poly1305_HEADERBYTES, MSG_WAITALL) != crypto_secretstream_xchacha20poly1305_HEADERBYTES) {close(sockClient); syslog(LOG_WARNING, "IntCom[SS] Failed receiving header"); continue;}
-		if (crypto_secretstream_xchacha20poly1305_init_pull(&ss_state, ss_header, intcom_key) != 0) {close(sockClient); syslog(LOG_WARNING, "IntCom[SS] Failed init"); continue;}
+		if (recv(AEM_FD_SOCK_CLIENT, ss_header, crypto_secretstream_xchacha20poly1305_HEADERBYTES, MSG_WAITALL) != crypto_secretstream_xchacha20poly1305_HEADERBYTES) {close(AEM_FD_SOCK_CLIENT); syslog(LOG_WARNING, "IntCom[SS] Failed receiving header"); continue;}
+		if (crypto_secretstream_xchacha20poly1305_init_pull(&ss_state, ss_header, intcom_key) != 0) {close(AEM_FD_SOCK_CLIENT); syslog(LOG_WARNING, "IntCom[SS] Failed init"); continue;}
 
 		unsigned char ss_tag = 0xFF;
 		unsigned char enc[AEM_SMTP_CHUNKSIZE + crypto_secretstream_xchacha20poly1305_ABYTES];
@@ -77,18 +74,18 @@ void intcom_serve_stream(void) {
 		size_t lenEnc = 0;
 
 		if (
-		   recv(sockClient, &lenEnc, sizeof(size_t), MSG_WAITALL) != sizeof(size_t)
+		   recv(AEM_FD_SOCK_CLIENT, &lenEnc, sizeof(size_t), MSG_WAITALL) != sizeof(size_t)
 		|| lenEnc != sizeof(struct emailMeta) + crypto_secretstream_xchacha20poly1305_ABYTES
-		|| recv(sockClient, enc, lenEnc, MSG_WAITALL) != (ssize_t)lenEnc
+		|| recv(AEM_FD_SOCK_CLIENT, enc, lenEnc, MSG_WAITALL) != (ssize_t)lenEnc
 		|| crypto_secretstream_xchacha20poly1305_pull(&ss_state, (unsigned char*)&dlv->meta, NULL, &ss_tag, enc, sizeof(struct emailMeta) + crypto_secretstream_xchacha20poly1305_ABYTES, NULL, 0) != 0
 		|| ss_tag != 0
-		|| recv(sockClient, &lenEnc, sizeof(size_t), MSG_WAITALL) != sizeof(size_t)
+		|| recv(AEM_FD_SOCK_CLIENT, &lenEnc, sizeof(size_t), MSG_WAITALL) != sizeof(size_t)
 		|| lenEnc != sizeof(struct emailInfo) + crypto_secretstream_xchacha20poly1305_ABYTES
-		|| recv(sockClient, enc, lenEnc, MSG_WAITALL) != (ssize_t)lenEnc
+		|| recv(AEM_FD_SOCK_CLIENT, enc, lenEnc, MSG_WAITALL) != (ssize_t)lenEnc
 		|| crypto_secretstream_xchacha20poly1305_pull(&ss_state, (unsigned char*)&dlv->info, NULL, &ss_tag, enc, sizeof(struct emailInfo) + crypto_secretstream_xchacha20poly1305_ABYTES, NULL, 0) != 0
 		|| ss_tag != 0
 		) {
-			close(sockClient);
+			close(AEM_FD_SOCK_CLIENT);
 			syslog(LOG_WARNING, "IntCom[SS] Failed receiving/decrypting metadata");
 			continue;
 		}
@@ -98,7 +95,7 @@ void intcom_serve_stream(void) {
 
 		while(1) {
 			// Receive size
-			if (recv(sockClient, &lenEnc, sizeof(size_t), 0) != sizeof(size_t)) {
+			if (recv(AEM_FD_SOCK_CLIENT, &lenEnc, sizeof(size_t), 0) != sizeof(size_t)) {
 				syslog(LOG_WARNING, "IntCom[SS] Failed receiving message length");
 				break;
 			}
@@ -114,7 +111,7 @@ void intcom_serve_stream(void) {
 			}
 
 			// Receive message
-			if (recv(sockClient, enc, lenEnc, MSG_WAITALL) != (ssize_t)lenEnc) {
+			if (recv(AEM_FD_SOCK_CLIENT, enc, lenEnc, MSG_WAITALL) != (ssize_t)lenEnc) {
 				syslog(LOG_WARNING, "IntCom[SS] Failed receiving message");
 				break;
 			}
@@ -131,13 +128,13 @@ void intcom_serve_stream(void) {
 
 		if (dlv->lenSrc > 1) {
 			const int32_t ret = deliverEmail(&dlv->meta, &dlv->info, dlv->src, dlv->lenSrc);
-			if (send(sockClient, &ret, sizeof(int32_t), 0) != sizeof(int32_t)) {
+			if (send(AEM_FD_SOCK_CLIENT, &ret, sizeof(int32_t), 0) != sizeof(int32_t)) {
 				syslog(LOG_ERR, "IntCom[SS]: Failed sending end-result: %m");
 			}
 		}
 
 		sodium_memzero(dlv, sizeof(struct dlvEmail));
-		close(sockClient);
+		close(AEM_FD_SOCK_CLIENT);
 	}
 
 	free(dlv);
